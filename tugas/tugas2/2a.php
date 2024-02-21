530b1531a6de763aed82bb625b47c85567407ac7
kresnasatria <kresnasatria160@gmail.com>
2024-02-21 18:30:21 +0700

    menyelesaikan tugas

--- tugas/tugas2/2a.php
+++ tugas/tugas2/2a.php
@@ -1,15 +1,15 @@
 <?php 
 
 $first_name = "Kresna";
-$middle_name ="Satria";
+$middle_names ="Satria";
 
 for ($i = 1; $i <= 100; $i++) {
     if ($i % 3 == 0 and $i % 5 == 0) {
-        echo $first_name . $middle_name . "<br>";
+        echo $first_name . $middle_names . "<br>";
     } else if ($i % 3 == 0) {
         echo $first_name . "<br>";
     } else if ($i % 5 == 0) {
-        echo $middle_name . "<br>";
+        echo $middle_names . "<br>";
     } else {
         echo $i . "<br>";
     }
--- tugas/tugas2/2d.php
+++ tugas/tugas2/2d.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas 2d Kresna</title>
+    <title>Tugas 2d Kresna Satria</title>
     <style>
         .flex {
             display: flex;
