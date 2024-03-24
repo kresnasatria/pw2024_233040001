8a8809d560ffaafa3dade0f043dd756cb4c9be5a
kresnasatria <kresnasatria160@gmail.com>
2024-03-24 07:42:31 +0700

    tugas 5

--- tugas/tugas5/5a.php
+++ tugas/tugas5/5a.php
@@ -4,52 +4,62 @@ $student = [
             ["nama" => "Siluman Kukang",
               "npm" => "233040001",
               "email" => "kukang@gmail.com ",
-              "jurusan" => "Teknik Informatika",           
+              "jurusan" => "Teknik Informatika",  
+              "gambar" => "1"         
             ],
             ["nama" => "Kambing Gunung",
               "npm" => "233040029",
               "email" => "mmbeee@gmail.com ",
               "jurusan" => "Kedokteran",
+              "gambar" => "2"
             ],
             ["nama" => "Lontong Tengkurep",
               "npm" => "233040035",
               "email" => "maudibalikjugasamaaja@gmail.com ",
-              "jurusan" => "Teknik Sipil",             
+              "jurusan" => "Teknik Sipil",  
+              "gambar" => "3"           
             ],
             ["nama" => "Kesatria Lunak",
               "npm" => "233040017",
               "email" => "tidakgagahtapilunak@gmail.com ",
               "jurusan" => "Hukum",
+              "gambar" => "4"
             ],
             ["nama" => "Jerapah Terbang",
               "npm" => "233040036",
               "email" => "leherpanjangbersayap@gmail.com ",
               "jurusan" => "Teknik Mesin",
+              "gambar" => "5"
             ],
             ["nama" => "Buaya Jingkrak",
               "npm" => "233040006",
               "email" => "untungbukanbuayadarat@gmail.com ",
               "jurusan" => "Perencanaan Wilayah dan Kota",
+              "gambar" => "6"
             ],
             ["nama" => "Banteng Loncat",
               "npm" => "233040009",
               "email" => "nocommentperbantengan@gmail.com ",
               "jurusan" => "Teknologi Pangan",
+              "gambar" => "7"
             ],
             ["nama" => "Sop Pinguin",
               "npm" => "233040105",
               "email" => "kejamamat@gmail.com ",
               "jurusan" => "Teknik Industri",
+              "gambar" => "8"
             ],
              ["nama" => "Lele Breakdance",
               "npm" => "233040125",
               "email" => "udahlenturdarilahir@gmail.com ",
               "jurusan" => "Teknik Lingkungan",
+              "gambar" => "9"
             ],
             ["nama" => "Mangga Berduri",
               "npm" => "233040002",
               "email" => "mampuskaubukanya@gmail.com ",
               "jurusan" => "Sistem Informasi",
+              "gambar" => "10"
             ]
 
             ];
@@ -67,8 +77,9 @@ $student = [
     }
     
     .container {
+        
         width: 500px;
-        height: 950px;
+        height: 2500px;
         background-color: salmon;
         padding: 5px;
         border: 2px solid black;
@@ -84,6 +95,7 @@ $student = [
         <h1>Daftar Mahasiswa</h1>
             <?php foreach ($student as $students) : ?>
                 <ul>
+                    <img src="https://randomuser.me/api/portraits/men/<?= $students['gambar'] ?>.jpg" width="150" height="150">
                     <li>Nama : <?= $students["nama"]; ?></li>
                     <li>NPM : <?= $students["npm"]; ?></li>
                     <li>Email : <?= $students["email"]; ?></li>
